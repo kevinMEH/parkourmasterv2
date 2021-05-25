3e0300bd18ded09ac9a6d711b3870848e24381e1
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-05-25 11:12:32 -0400

    Added new map assets

---
+++ core/assets/maps/environmentv2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="environmentv2" tilewidth="4" tileheight="4" tilecount="2500" columns="50">
+ <image source="environmentv2.png" width="200" height="200"/>
+</tileset>
