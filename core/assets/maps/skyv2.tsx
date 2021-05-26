091b1caf3d6d1eb07ea78912ebcef4ee1c9289ee
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-05-26 11:58:00 -0400

    Updated map

---
+++ core/assets/maps/skyv2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="skyv2" tilewidth="4" tileheight="4" tilecount="11250" columns="150">
+ <image source="background.png" width="600" height="300"/>
+</tileset>
