b65709b514704beba64877d26292773acfb4aedf
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-06-08 15:37:33 -0400

    File cleanup

--- docs/assets/maps/sky-5ab2342180e2f134a23beb1287d40e65.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="sky" tilewidth="4" tileheight="4" tilecount="90000" columns="400">
- <image source="sky.png" width="1600" height="900"/>
-</tileset>
