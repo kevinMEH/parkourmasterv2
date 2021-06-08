3a3c1971c7ab559dcece03af2fc1848b5b45f4ff
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2021-06-08 13:28:03 -0400

    Removed useless stuff

---
+++ html/build/gwt/gen/com/google/gwt/lang/io_00046github_00046kevinmeh_00046GdxDefinition__EntryMethodHolder.java
@@ -0,0 +1,6 @@
+package com.google.gwt.lang;
+public class io_00046github_00046kevinmeh_00046GdxDefinition__EntryMethodHolder {
+  public static final void init() {
+    // to be synthesized later
+  }
+}
